a654633952e02ee254697d5d955078bea1cc1f90
Roman Broshnitskiy <romario777v8@gmail.com>
2024-03-17 22:31:03 +0200

    add error interceptop

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ErrorInterceptor } from './error.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalInterceptors(new ErrorInterceptor());
 
   await app.listen(3000);
   console.info(`Application is running on: ${await app.getUrl()}`);
